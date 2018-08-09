db354f17f4e2a25849ebf89a36c1dde2c1b0f65f
Albin Haglund <haggespel@gmail.com>
2018-08-09 11:55:47 +0100

    test

---
+++ LD42/Assets/Managers/GameManager/gamemanager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamemanager : MonoBehaviour {
+
+    public static gamemanager instance;
+
+    void Awake () {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+	
+}
