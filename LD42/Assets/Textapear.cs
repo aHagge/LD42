fde6fa1c2221c89ae769c39c8102d094fc6d7142
Albin Haglund <haggespel@gmail.com>
2018-08-11 11:19:44 +0100

    fourth

---
+++ LD42/Assets/Textapear.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Textapear : MonoBehaviour {
+
+    public float delay;
+
+    public string day;
+
+    private string currenttext;
+	// Use this for initialization
+	void Start () {
+        day = "Day 1";
+
+        StartCoroutine(show());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator show()
+    {
+        for (int i = 0; i < day.Length + 1; i++)
+        {
+            currenttext = day.Substring(0, i);
+            gameObject.GetComponent<TextMeshProUGUI>().text = currenttext;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
